e77b48bfd26e0737e94dacdb98b022e3b85af2d8
Kalid-N <107488332+Mckali-2k@users.noreply.github.com>
2022-08-04 21:22:52 +0300

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_userName = new System.Windows.Forms.Label();
+            this.txt_userName = new System.Windows.Forms.TextBox();
+            this.txt_pswd = new System.Windows.Forms.TextBox();
+            this.lbl_pswd = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_userName
+            // 
+            this.lbl_userName.AutoSize = true;
+            this.lbl_userName.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_userName.Location = new System.Drawing.Point(90, 66);
+            this.lbl_userName.Name = "lbl_userName";
+            this.lbl_userName.Size = new System.Drawing.Size(173, 33);
+            this.lbl_userName.TabIndex = 0;
+            this.lbl_userName.Text = "User Name:";
+            // 
+            // txt_userName
+            // 
+            this.txt_userName.Location = new System.Drawing.Point(280, 75);
+            this.txt_userName.Name = "txt_userName";
+            this.txt_userName.Size = new System.Drawing.Size(307, 22);
+            this.txt_userName.TabIndex = 1;
+            // 
+            // txt_pswd
+            // 
+            this.txt_pswd.Location = new System.Drawing.Point(280, 166);
+            this.txt_pswd.Name = "txt_pswd";
+            this.txt_pswd.Size = new System.Drawing.Size(307, 22);
+            this.txt_pswd.TabIndex = 3;
+            // 
+            // lbl_pswd
+            // 
+            this.lbl_pswd.AutoSize = true;
+            this.lbl_pswd.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pswd.Location = new System.Drawing.Point(90, 157);
+            this.lbl_pswd.Name = "lbl_pswd";
+            this.lbl_pswd.Size = new System.Drawing.Size(157, 33);
+            this.lbl_pswd.TabIndex = 2;
+            this.lbl_pswd.Text = "Password:";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(174, 252);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(89, 38);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reset.Location = new System.Drawing.Point(352, 252);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(89, 38);
+            this.btn_reset.TabIndex = 5;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(654, 389);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_pswd);
+            this.Controls.Add(this.lbl_pswd);
+            this.Controls.Add(this.txt_userName);
+            this.Controls.Add(this.lbl_userName);
+            this.Name = "Form2";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_userName;
+        private System.Windows.Forms.TextBox txt_userName;
+        private System.Windows.Forms.TextBox txt_pswd;
+        private System.Windows.Forms.Label lbl_pswd;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        //public static string userName;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string userName = txt_userName.Text;
+
+            if ( txt_userName.Text == "admin" && txt_pswd.Text == "admin")
+            {
+                this.Hide();
+
+                Form1 f1 = new Form1(userName);
+                f1.Show();
+            }
+                
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
