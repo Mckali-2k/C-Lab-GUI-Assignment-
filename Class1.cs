baa86a004f5c97693a889e35c3583093630a1e86
Kalid-N <107488332+Mckali-2k@users.noreply.github.com>
2022-08-04 21:13:46 +0300

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +9,51 @@ namespace WindowsFormsApp2
 {
     class Class1
     {
-        private string name, date, objName, sku;
-        private  int count;
-        private double price;
+        private string name, date, objName, inventoryNumber;
+        private  string count;
+        private string price;
+        private bool isAvailable;
+        private bool discount;
+        private bool storeCredit;
+        private bool delivery;
+        private bool coupon;
+        private string options = "";
+        private bool telebir;
+
+        static List<Class1> class1s = new List<Class1>(); // 1 instance
+
+        public string OPTIONS
+        {
+            set { options = value; }
+            get { return options; }
+        }
+
+        public bool ISAVILABLE
+        {
+            set { isAvailable = value; }
+            get { return isAvailable; }
+        }
+        public bool STORECREDIT
+        {
+            set { storeCredit = value; }
+            get { return storeCredit; }
+        }
+        public bool DELIVERY
+        {
+            set { delivery = value; }
+            get { return delivery; }
+        }
+        public bool COUPON
+        {
+            set { coupon = value; }
+            get { return coupon; }
+        }
+        public bool DISCOUNT
+        {
+            set { discount = value; }
+            get { return discount; }
+        }
+
 
         public string NAME
         {
@@ -31,40 +73,50 @@ namespace WindowsFormsApp2
             get { return objName; }
         }
 
-        public string SKU
+        public string INVENTORYNUMBER
         {
-            set { sku = value; }
-            get { return sku; }
+            set { inventoryNumber = value; }
+            get { return inventoryNumber; }
         }
 
-        public int COUNT
+        public string COUNT
         {
             set { count = value; }
             get { return count; }
         }
 
-        public double PRICE
+        public string PRICE
         {
             set { price = value; }
             get { return price; }
         }
 
 
-        public Class1(string name, string date, string objName, string sku, int count, double price)
+        public Class1(string name, string date, string objName, string inventoryNumber, string count, string price)
         {
             this.name = name;
             this.date = date;
             this.objName = objName;
-            this.sku = sku;
+            this.inventoryNumber = inventoryNumber;
             this.count = count;
             this.price = price;
         }
 
         public Class1() { }
 
+        public List<Class1> getAll()
+        {
+            return class1s;
+        }
+
         public void save()
         {
-            MessageBox.Show("Succesfully Added");
+           /* var products = new List<string>();
+            products.Add(this.price);*/
+
+            class1s.Add(this);
+
+     /*       MessageBox.Show("Succesfully Added");*/
         }
     }
 }
